34f08ce9a78f241649571be228ccbca649b6303a
caa055 <art.nas@yahoo.com>
2026-06-08 22:26:49 -0500

    Automated product sync

--- ConnectGit.bat
+++ ConnectGit.bat
@@ -19,3 +19,15 @@ git remote add origin https://github.com/caa055/WearADareFunnel.git
 :: Push the code to the cloud
 git branch -M main
 git push -u origin main
+
+:: ----- IF ERRORS THEN FORCE IT:
+:: git remote set-url origin https://github.com/caa055/WearADareFunnel.git
+:: git push -u origin main
+
+:: ------ AFTER IT'S ALL WORKING THEN:
+:: Go to your repository page on the GitHub website.
+:: Click on Settings (the gear icon at the top menu bar.
+:: On the left sidebar, click on Pages.
+:: Under Build and deployment -> Source, click the dropdown and change it from "Deploy from a branch" to "GitHub Actions".
+
+::  https://caa055.github.io/WearADareFunnel/
